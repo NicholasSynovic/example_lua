4d6199b4eb58501c0cd4086eb1ba8278576d5bf3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-01-12 17:10:41 -0600

    Add comments

--- src/integer_arithmetic.lua
+++ src/integer_arithmetic.lua
@@ -11,7 +11,7 @@ local function product(a, b)
 end
 
 local function int_quotient(a, b)
-    return a // b
+    return a // b -- Rounds to negative infinity
 end
 
 local function remainder(a, b)
@@ -23,19 +23,21 @@ local function exponentiation(a, b)
 end
 
 local function main()
-    io.write("First number: ")
-    local a = io.read("n")
+    io.write("First number: ") -- Use with io.read for single line input
+    local a = io.read("n") -- Captures user input
 
     io.write("Second number: ")
     local b = io.read("n")
 
     print("===")
 
-    print("Sum: " .. sum(a, b))
+    print("Sum: " .. sum(a, b)) -- ".." syntax used to concatenate
     print("Difference: " .. difference(a, b))
     print("Product: " .. product(a, b))
-    print("Integer Quotient: " .. int_quotient(a, b))
-    print("Remainder: " .. remainder(a, b))
+    print(
+        "Integer Quotient (rounds to negative infinity): " .. int_quotient(a, b)
+    )
+    print("Remainder" .. remainder(a, b))
     print("Exponentiation: " .. exponentiation(a, b))
 end
 
